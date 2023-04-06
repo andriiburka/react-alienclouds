23d1f89c47924290783ef3762d5a94b3fb39fef1
Andrii Burka <andriiburka@gmail.com>
2023-04-06 19:45:55 +0300

    Looks OK, TODO: Auth logic

---
+++ src/components/Scripts.js
@@ -0,0 +1,10 @@
+export default function Scripts() {
+    return (
+        <div>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/2.26.2/js/uikit.min.js'></script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/2.26.2/js/components/sticky.min.js'></script>
+        </div>
+
+    )
+}
---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+
+
+function Welcome () {
+
+    return (
+        <div className="welcome-block">
+            <div id="cube" className="cube magenta">
+                <div className="cube__inner">
+                    <div className="cube__side front"></div>
+                    <div className="cube__side back"></div>
+                    <div className="cube__side top"></div>
+                    <div className="cube__side bottom"></div>
+                    <div className="cube__side left"></div>
+                    <div className="cube__side right"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ export default Welcome
