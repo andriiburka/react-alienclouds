3e7f6dddaee4df8511587ebd24636cfd83aa3a82
andriiburka <andriiburka@gmail.com>
2023-11-13 22:20:19 +0200

    CSS + Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#Alienclouds
+![catalog.png](src%2Fimages%2Fscreenshots%2Fcatalog.png)
+![details.png](src%2Fimages%2Fscreenshots%2Fdetails.png)
+![login.png](src%2Fimages%2Fscreenshots%2Flogin.png)
+![404.png](src%2Fimages%2Fscreenshots%2F404.png)
+![204.png](src%2Fimages%2Fscreenshots%2F204.png)
--- src/components/partials-components/Err404.jsx
+++ src/components/partials-components/Err404.jsx
@@ -1,9 +1,16 @@
+import {NavLink} from "react-router-dom";
+import React from "react";
+
 function Err404() {
     return (
         <div className="auth-box infinite-glow" style={{border: "none"}}>
-            <h1 style={{fontSize: "100px", backgroundColor: "transparent", top: "50%"}}
-                className="projects-area">404</h1>
-            <h2 style={{backgroundColor: "transparent"}}>Page not found</h2>
+            <h1 style={{fontSize: "3em", backgroundColor: "transparent", top: "50%"}}>404</h1>
+            <h2 style={{backgroundColor: "transparent"}}>Aliens doesn't exist</h2>
+            <div className="auth-buttons-container">
+                <div className="glow-purple-button">
+                    <NavLink className="btn text-center" to="/">HOME</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
